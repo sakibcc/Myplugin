8d2e421f3168c2d37889824742b0d76197834853
sakibcc <ud.k2007@gmail.com>
2018-03-03 20:08:49 +0800

    firstcommit

---
+++ fullpage.js
@@ -0,0 +1,45 @@
+;(function (window,$) {
+	$.fn.fullpage = function (options) {
+		options = $.extend({
+			pageClass : "pages",
+			maxPage : 5
+		},options);
+		
+		var tag = 0,
+			_this = $(this);
+		//配置关键样式
+		_this .css({
+			position: 'relative',
+			top: '0'
+		});
+		var $sections = _this .children('.'+options.pageClass);
+		$sections.css('width', '100%');
+
+		$sections.css('height', window.innerHeight+'px');
+		//绑定resize事件
+		$(window).resize(function(event) {
+	        $sections.css('height', window.innerHeight+'px');
+	        _this .css('top', '-'+ window.innerHeight * tag +'px');
+   		 });
+
+		$(window).keydown(function(event) {
+	       if(event.keyCode == 40) {
+	            if(tag !== options.maxPage - 1){
+	                if(!_this .is(":animated")){
+	                    tag += 1;
+	                    _this .animate({top : "-="+$sections[0].offsetHeight}, 1000)
+	                }
+	            }
+	       }else if(event.keyCode == 38) {
+	           if(tag !== 0){         
+	                if(!_this .is(":animated")){
+	                    tag -= 1;
+	                    _this .animate({top : "+="+$sections[0].offsetHeight}, 1000)
+	                }
+	            }
+	       }
+	    });
+
+		return this;
+	}
+}(window,jQuery))
