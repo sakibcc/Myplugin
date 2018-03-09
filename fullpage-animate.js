27b937ef9b7996f705d2ec653c7eca6a09f54b63
sakibcc <ud.k2007@gmail.com>
2018-03-10 00:03:37 +0800

    fix some bug

--- fullpage-animate.js
+++ fullpage-animate.js
@@ -8,62 +8,98 @@
 			maxPage : 5,
 			speed : 1000
 		},options);
-		
-		var tag = 0,
-			_this = $(this);
+
+		var _tag = 0,
+			$this = $(this);
+		//定义触摸移动时的坐标
+		var startY,
+			moveY;
+
+		//根据当前页面情况获取tag值
+		function flashTag() {
+			var $p = $('.pages').css('height'),
+				$f = $('.fullpage').css('top');
+			_tag = Math.abs($f.split('px')[0]) / $p.split('px')[0];
+		}
+
+		//向下滑动
+		function _next() {
+			//边界判断
+			flashTag();
+			if(_tag !== options.maxPage - 1){
+			    if(!$this .is(":animated")){
+			        $this .animate({top : "-="+$sections[0].offsetHeight}, options.speed);
+			    }
+			}
+		}
+		//向上滑动
+		function _prev() {
+			//边界判断
+			flashTag();
+			if(_tag !== 0){         
+			     if(!$this .is(":animated")){
+			         _tag -= 1;
+			         $this .animate({top : "+="+$sections[0].offsetHeight}, options.speed);
+			     }
+			 }
+		}
 
 		//配置关键样式
 		$('body').css('overflow', 'hidden');
-		_this .css({
+		$this .css({
 			position: 'relative',
 			top: '0'
 		});
-		var $sections = _this .children('.'+options.pageClass);
+		var $sections = $this.children('.'+options.pageClass);
 		$sections.css('width', '100%');
 		$sections.css('height', window.innerHeight+'px');
 
 		//绑定resize事件
 		$(window).resize(function(event) {
+			flashTag();
 	        $sections.css('height', window.innerHeight+'px');
-	        _this .css('top', '-'+ window.innerHeight * tag +'px');
+	        $this .css('top', '-'+ window.innerHeight * _tag +'px');
    		 });
 		//绑定keydowm事件
 	    $(window).on(
-	    	'keydown' , function(event) {
+	    	'keydown' , function(e) {
+	    		var event = e || window.event;
 		    	 if(event.keyCode == 40) {
-		            if(tag !== options.maxPage - 1){
-		                if(!_this .is(":animated")){
-		                    tag += 1;
-		                    _this .animate({top : "-="+$sections[0].offsetHeight}, options.speed);
-		                }
-		            }
+		            _next();
 		       	 }else if(event.keyCode == 38) {
-		           if(tag !== 0){         
-		                if(!_this .is(":animated")){
-		                    tag -= 1;
-		                    _this .animate({top : "+="+$sections[0].offsetHeight}, options.speed);
-		                }
-		            }
+		           	_prev();
 		         }
 	   		 });
 	    //绑定滚轮事件
-	    $(window).on('mousewheel DOMMouseScroll', function(event) {
+	    $(window).on('mousewheel DOMMouseScroll', function(e) {
+	    	var event = e || window.event;
 	    	//兼容firefox
 	    	var delta = event.originalEvent.detail !== 0 ? -event.originalEvent.detail : event.originalEvent.wheelDelta;
 	    	if(delta < 0) {
-	    		if(tag !== options.maxPage - 1){
-	    		    if(!_this .is(":animated")){
-	    		        tag += 1;
-	    		        _this .animate({top : "-="+$sections[0].offsetHeight}, options.speed);
-	    		    }
+	    		if(_tag !== options.maxPage - 1){
+	    		    _next();
 	    		}
 	    	}else if(delta > 0) {
-	    		if(tag !== 0){         
-	    		       if(!_this .is(":animated")){
-	    		           tag -= 1;
-	    		           _this .animate({top : "+="+$sections[0].offsetHeight}, options.speed);
-	    		       }
+	    		if(_tag !== 0){         
+	    		     _prev();
 	    		 }
+	    	}
+	    });
+	    //绑定触屏滑动事件
+	    $(window).on('touchstart', function(e) {
+	    	var event = e || window.event;
+	    	event.preventDefault();
+	    	startY = event.originalEvent.changedTouches[0].pageY;
+	    });
+	    $(window).on('touchmove', function(e) {
+	    	var event = e || window.event;
+	    	event.preventDefault();
+	    	moveY = event.originalEvent.changedTouches[0].pageY;
+	    	var Y = moveY - startY;
+	    	if(Y > 0){
+	    		_prev();
+	    	}else if(Y < 0) {
+	    		_next();
 	    	}
 	    });
 	}
